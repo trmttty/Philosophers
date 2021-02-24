5a3823d6da8f2a945f56df52f151260b070192aa
trmttty <tatsuya.tarumoto@gmail.com>
2021-02-24 21:43:01 +0900

    add

---
+++ Philosophers/philo_new/exit.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttarumot <ttarumot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/21 23:51:39 by ttarumot          #+#    #+#             */
+/*   Updated: 2021/02/24 20:34:20 by ttarumot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_two.h"
+
+int     error_status(char *message)
+{
+    if (message)
+        ft_putendl_fd(message, 2);
+    return (1);
+}
+
+void    exit_status(char *message, int code)
+{
+    if (message)
+        ft_putendl_fd(message, 2);
+    exit(code);
+}
+
+void    error_exit(t_state *state, char *message)
+{
+    state->thread_error = 1;
+	sem_wait(state->sem_display);
+    if (message)
+        ft_putendl_fd(message, 2);
+    exit(1);
+}
